d65d83c0594fb586666916b8330a7d5ea99d7df3
Altamir <alt.coelho@gmail.com>
2018-07-21 23:14:08 -0500

    faculty content

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,7 +3,13 @@ import AbtLogo from '../images/abt-logo.png'
 import SallyImg from '../images/site/sally.jpg'
 import AshleyImg from '../images/site/ashley.jpg'
 import VeronicaImg from '../images/site/veronica.jpg'
+import NickyImg from '../images/site/nicky.jpg'
 import AmandaImg from '../images/site/amanda.jpg'
+import AlannaImg from '../images/site/alanna.jpg'
+import SarahImg from '../images/site/sarah.jpg'
+import MeganImg from '../images/site/megan.jpg'
+import EdwinImg from '../images/site/edwin.jpg'
+import DevinImg from '../images/site/devin.jpg'
 import Minion404Img from '../images/site/minion404.jpg'
 import MovementTherapyImg from '../images/site/daniel-molly-rossberg-sally-hart.jpg'
 import SpringPerformanceImg from '../images/performances/piedpiper-compressed.jpg'
@@ -14,7 +20,13 @@ export const Logo = () => <img className="logo-image" src={AbtLogo} alt="abt log
 export const SallyImage = () => <img src={SallyImg} alt="Sally Hart" title="Sally Hart" />
 export const VeronicaImage = () => <img src={VeronicaImg} alt="Veronica Piller" title="Sally Hart" />
 export const AshleyImage = () => <img src={AshleyImg} alt="Ashley Geisler" title="Ashley Geisler" />
+export const NickyImage = () => <img src={NickyImg} alt="Nicky Coelho" title="Nicky Coelho" />
 export const AmandaImage = () => <img src={AmandaImg} alt="Amanda Jo Parks" title="Amanda Jo Parks" />
+export const AlannaImage = () => <img src={AlannaImg} alt="Alanna Mahon " title="Alanna Mahon" />
+export const SarahImage = () => <img src={SarahImg} alt="Sarah Gillespie" title="Sarah Gillespie" />
+export const MeganImage = () => <img src={MeganImg} alt="Megan Hillary" title="Megan Hillary" />
+export const EdwinImage = () => <img src={EdwinImg} alt="Edwin Roa" title="Edwin Roa" />
+export const DevinImage = () => <img src={DevinImg} alt="Devin Sweet" title="Devin Sweet" />
 export const Minion404 = () => <img src={Minion404Img} alt="Minion 404" title="Minion 404" />
 export const MovementTherapyImage = () => <img src={MovementTherapyImg} alt="Movement Therapy Classes" title="Movement Therapy Classes" />
 export const SpringPerformanceImage = () => <img src={SpringPerformanceImg} alt="spring performance poster" title="spring performance poster" />
--- src/components/faculty/Faculty.js
+++ src/components/faculty/Faculty.js
@@ -1,67 +1,238 @@
 import React from 'react'
-import { SallyImage, AshleyImage, VeronicaImage, AmandaImage } from '../Images'
+import {
+  SallyImage,
+  AshleyImage,
+  VeronicaImage,
+  AmandaImage,
+  NickyImage,
+  AlannaImage,
+  SarahImage,
+  MeganImage,
+  EdwinImage,
+  DevinImage,
+  } from '../Images'
 import './Faculty.css'
 
 /* eslint-disable max-len */
 const Faculty = () => (
   <div className="faculty-container">
     <div className="bios">
+      <p className="bold">Sally Hart - Owner/Director </p>
       <SallyImage />
       <p>
-          Director <span className="bold">Sally Hart</span>, formerly danced with the Joffrey Ballet under the direction of
-          Robert Joffrey. She performed at the grand opening of the Kennedy Center, with Kevin McKenzie, and in Joffrey Ballet
-          classics Petrouchka, Trinity, Viva Vivaldi, and The Seasons.
+        A former dancer with the Joffrey Ballet under the direction of Robert
+        Joffrey. She performed at the grand opening of the Kennedy Center, with
+        Kevin McKenzie, and in Joffrey Ballet classics Petrouchka, Trinity, Viva
+        Vivaldi, and The Seasons.
       </p>
       <p>
-          After her dance career, Mrs. Hart went on to become a professional make-up artist and did work on movie sets, runway
-          shows, and for modeling agencies. Mrs. Hart began teaching ballet locally in 1992. She has produced many local dance
-          productions. Sally has trained and mentored numerous students who have become professional dancers. Her unique curriculum includes
-          a blend of English, Italian, and Russian technique.
+        After her dance career, Mrs. Hart went on to become a professional
+        make-up artist and worked on movie sets, runway shows, and for modeling
+        agencies. Mrs. Hart began teaching ballet locally in 1992. She has
+        produced many local dance productions. Sally has trained and mentored
+        numerous students who have become professional dancers. Her unique
+        curriculum includes a blend of English, Italian, and Russian technique.
       </p>
       <p>
-        <span className="italic bold">“If you are looking for a dance instructor for your fledgling child or aspiring professional, Mrs.
-      Sally Hart is the teacher that you want to train with!”- Linda M. Kuchera-Gage, formerly with “American Ballet Theatre”</span>
+        <span className="italic small">
+          “If you are looking for a dance instructor for your fledgling child or
+          aspiring professional, Mrs. Sally Hart is the teacher that you want to
+          train with!”- Linda M. Kuchera-Gage, formerly with “American Ballet
+          Theatre”
+        </span>
       </p>
     </div>
     <hr />
     <div className="bios">
-      <p>
-        <AshleyImage />
-        <span className="bold">Ashley Geisler</span> is an instructor and choreographer at ABT, who has danced with concorDance
-          contemporary. Mrs. Geisler studied with the Old Dominion Ballet Company in Norfolk, Virginia, under the husband and
-          wife team of Istvan and Nelli Ament.She spent her summers training with Nina Sakhnovskaya of the Vaganova Academy.
-      </p>
-      <p>
-          During her tenure at ODU Ballet, Ashley performed the roles of Marzipan, Chinese, and Russian in The Nutcracker,
-          and the lead roles in performances of Snow White and The Fairy Doll. After completing her undergraduate and graduate
-          work in Russian Language and Literature at the University of Virginia, Mrs. Geisler returned to ballet at Albemarle
-          Ballet Theatre. She has been teaching and choreographing at ABT since 2006.
-      </p>
-    </div>
-    <hr />
-    <div className="bios">
+      <p className="bold">Veronica Piller - Artistic Director </p>
       <p>
         <VeronicaImage />
-        <span className="bold">Veronica Piller</span> is the artistic director, as well as an instructor and choreographer at
-          ABT. Mrs. Piller is the founder and director of concorDance contemporary, a professional contemporary dance company. She
-          formerly danced with the Charleston Ballet Theatre, San Diego Ballet, The Garage Contemporary Ballet, Charlottesville
-          Ballet, and Keith Lee Dances.
+        Mrs. Piller is the founder and director of concorDance contemporary, a
+        professional contemporary dance company. She formerly danced with the
+        Charleston Ballet Theatre, San Diego Ballet, The Garage Contemporary
+        Ballet, Charlottesville Ballet, and Keith Lee Dances.
       </p>
       <p>
-        Veronica began her training with her mother, Sally Hart. She continued her dance education at the Virginia School of the
-        Arts and the Joffrey Ballet School and was blessed to have been the recipient of many scholarships.
+        Veronica began her training with her mother, Sally Hart. She continued
+        her dance education at the Virginia School of the Arts and the Joffrey
+        Ballet School and was blessed to have been the recipient of many
+        scholarships.
       </p>
-      <p>Veronica has performed leading roles in <span className="italic">Cinderella, Paquita, Les Corsaire, Les Sylphedes, and Swan Lake</span>, and choreographers have created roles for her. Additional memorable performances include <span className="italic">The King and I wiith Ash Lawn Opera, with the Duke Ellington Orchestra, Maria in West Side Story, and Keith Lee’s Mama Rose.</span>
+      <p>
+        Veronica has performed leading roles in
+        <span className="italic">
+          Cinderella, Paquita, Les Corsaire, Les Sylphedes, and Swan Lake
+        </span>, and choreographers have created roles for her. Additional
+        memorable performances include
+        <span className="italic">
+          The King and I wiith Ash Lawn Opera, with the Duke Ellington
+          Orchestra, Maria in West Side Story, and Keith Lee’s Mama Rose.
+        </span>
       </p>
     </div>
     <hr />
     <div className="bios">
+      <p className="bold">Ashley Geisler - Director of Operations/Instructor</p>
       <p>
-        <AmandaImage />
-        <span className="bold">Amanda Jo Parks</span> received her early dance training under the direction of Christine Meneer and then received a scholarship to study at the Dance Institute at the University of Akron. She spent summers training at the Pittsburgh Ballet Theatre, Pennsylvania Ballet, Chautauqua Dance Festival and Ballet Austin. Amanda performed professionally with the Pittsburgh Ballet, BalletMet, and the Ohio Ballet appearing in productions such as George Balanchine’s Rubies, Dwight Rhoden’s StrayLifeLushHorn, Ruthanna Boris’s Cakewalk, as well as in contemporary classics, such as Beauty and the Beast, Cleopatra, and countless Nutcrackers.
+        <AshleyImage />
+        Mrs. Geisler studied with the Old Dominion Ballet Company in Norfolk,
+        Virginia, under the husband and wife team of Istvan and Nelli Ament.She
+        spent her summers training with Nina Sakhnovskaya of the Vaganova
+        Academy.
       </p>
       <p>
-            Amanda went on to earn her undergraduate degree in journalism and then a PhD in political science from the Pennsylvania State University. She now enjoys teaching both ballet and political science but spends most of her time wrangling her three wild and delightful little boys.
+        During her tenure at ODU Ballet, Ashley performed the roles of Marzipan,
+        Chinese, and Russian in The Nutcracker, and the lead roles in
+        performances of Snow White and The Fairy Doll. After completing her
+        undergraduate and graduate work in Russian Language and Literature at
+        the University of Virginia, Mrs. Geisler returned to ballet at Albemarle
+        Ballet Theatre. She has been teaching and choreographing at ABT since
+        2006 and has performed with concorDance contemporary.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">
+        Nicky Coelho - Executive Associate/Production Manager
+      </p>
+      <p>
+        <NickyImage />
+        Mrs. Coelho began dancing with her mother Sally Hart. She has also
+        studied with Virginia School of the Arts, Maryland Youth Ballet, and on
+        scholarship at the Joffrey Ballet. Nicky has danced with Charleston
+        Ballet Theater, Robert Ivey Ballet, Mikerline Haitian Folkloric Dance
+        Company, and James Sewell Ballet where she was also the Company Manager
+        for two years. She has enjoyed working with choreographers Sharon Eyal,
+        Joanna Kotze, Belinda McGuire, Norbert De La Cruz III, Carl Flink, and
+        Myron Johnson to name a few.
+      </p>
+      <p>
+        Mrs. Coelho was on faculty at the Reif Center Summer Intensive in Grand
+        Rapids, MN for five years and directed the Intensive in Minneapolis in
+        2015.
+      </p>
+      <p>
+        Nicky has been the production and stage manager for ABT since 2005 and
+        has choreographed several full-length ballets and original works in the
+        repertoire.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Amanda Jo Parks - Instructor </p>
+      <p>
+        <AmandaImage />
+        Mrs. Parks received her early dance training under the direction of
+        Christine Meneer and then received a scholarship to study at the Dance
+        Institute at the University of Akron. She spent summers training at the
+        Pittsburgh Ballet Theatre, Pennsylvania Ballet, Chautauqua Dance
+        Festival and Ballet Austin. Amanda performed professionally with the
+        Pittsburgh Ballet, BalletMet, and the Ohio Ballet appearing in
+        productions such as George Balanchine’s Rubies, Dwight Rhoden’s
+        StrayLifeLushHorn, Ruthanna Boris’s Cakewalk, as well as in contemporary
+        classics, such as Beauty and the Beast, Cleopatra, and countless
+        Nutcrackers.
+      </p>
+      <p>
+        Amanda went on to earn her undergraduate degree in journalism and then a
+        PhD in political science from the Pennsylvania State University. She now
+        enjoys teaching both ballet and political science but spends most of her
+        time wrangling her three wild and delightful little boys.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Alanna Mahon - Instructor</p>
+      <p>
+        <AlannaImage />
+        Alanna is studio alumni and now instructor and choreographer at ABT.
+        Alanna trained at The Joffrey Ballet‘s Jazz/Modern school, The Hip Hop
+        Conservatory in NYC, and The Institute For American Musical Theatre.
+        Crozet native, & multi-medium artist, Alanna has performed with Live
+        Arts Theatre, concorDance contemporary, Phoenix Theatre Works and worked
+        as Ash Lawn Opera‘s production Intern. She‘s done background work on
+        network TV shows such as Homeland, and Impractical Jokesters. Alanna
+        helped choreograph and stage manage Peter Pan the Musical for Bronxville
+        School in New York. She also teaches children‘s Folktale style
+        storytelling at Wildrock, and sings lead vocals with the Charlottesville
+        band, 180.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Sarah Gillespie - Guest Instructor </p>
+      <p>
+        <SarahImage />
+        Sarah is a tango instructor at ABT. Mrs. Gillespie began her dance
+        training in classical ballet under Tristi Ann McMaster Robinson at her
+        studio in Richmond Virginia. She continued studying ballet throughout
+        high school and while at James Madison University where she earned her
+        undergraduate degree in Spanish. Mrs. Gillespie’s love of Spanish led
+        her to Buenos Aires Argentina where she discovered Argentine Tango in
+        2012. While living in Buenos Aires she studied privately with
+        instructors Javier Rochwarger and Anabella Cruz Heck. After earning her
+        Masters in Spanish Literature from Middlebury College in 2014, Mrs.
+        Gillespie returned to Virginia and began teaching Spanish at the Miller
+        School of Albemarle. Mrs. Gillespie is also a festival organizer,
+        milonga host, and instructor at Charlottesville Tango.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Megan Hillary - Guest Instructor</p>
+      <p>
+        <MeganImage />
+        A returning teacher for Acting for Dancers, Megan holds degrees in
+        visual art and technical theater design, though she grew up with ballet
+        until college, when she discovered modern and was wholly seduced by it.
+        Extremely active in the local gallery scene as an installation artist,
+        she also embraces directing and choreography -- at Live Arts and area
+        schools. Often to be found as a guest artist for puppetry at schools and
+        for children‘s and teen‘s organizations, Megan‘s actual day job is as
+        the new manager of The Hip Joint in downtown Charlottesville.
+      </p>
+      <br />
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Edwin Roa - Guest Instructor </p>
+      <p>
+        <EdwinImage />
+        A native of Bogota, Colombia, Edwin O. Roa moved to the US in 1995. He
+        is a professional dancer who has trained in multiple dance disciplines
+        and he currently teaches ballroom dances at James Madison University.
+        Edwin devoted his career to the study of partner dance. He has been
+        involved in our dance community for over 20 years. Edwin currently
+        resides in Charlottesville, VA, where he works as an independent dance
+        instructor. He is the founder of The Dance Spot and Co-founder of the
+        Charlottesville Salsa Club. His interest in music, cultures and the
+        perspectives of partner dance have encouraged him to experiment and
+        develop a method of teaching social partner dance called Zabor Dance.
+      </p>
+    </div>
+    <hr />
+    <div className="bios">
+      <p className="bold">Devin Sweet - Guest Instructor </p>
+      <p>
+        <DevinImage />
+        A native of upstate New York, Devin began his dance training with the
+        Fokine Ballet Company at the age of 4. He attended college at the
+        University of North Carolia School of the Arts. During his 4 years at
+        UNCSA, he performed works by world renowned choreographers such as
+        George Balanchine’s “Serenade”, Jermone Robbin’s “Fanfare”, and Sir
+        Fredrick Ashton’s “Les Patineurs”. He was selected to perform at the
+        Guggenheim museum in New York City where he performed August
+        Bournonville’s “Jockey Dance”. He was trained and coached for these
+        roles by the expert faculty at UNCSA including Ethan Steifel, Melissa
+        Hayden, Warren Conover, Frank Smith, Gyula, Pandi, Christine Kobborg,
+        Kat Wildish, and Slawomir Wozniak. As a professional, Devin has danced
+        both contemporary and ballet works. Performing as both a freelance
+        artist and company dancer, he has danced across the East Coast. Devin
+        started teaching in 2010 and has since been an active faculty member and
+        guest instructor at schools throughout Virginia and North Carolina. His
+        students have gone on to study with Richmond Ballet, Boston Ballet,
+        American Ballet Theatre, The Joffrey Ballet, New York City Ballet, and
+        many more.
       </p>
     </div>
     <hr />

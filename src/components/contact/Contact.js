2bd76e97670b6730636f3c2b032f6e48b35bb09c
Altamir <alt.coelho@gmail.com>
2018-08-07 16:19:20 -0500

    new site images

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,11 +7,17 @@ import './Contact.css'
 /* eslint-disable max-len */
 const Contact = () => (
   <div className="contact-container">
+    <div className="bg-image contact-img" />
     <div className="content">
       <p className="heading">Contact us:</p>
       <div id="wrap">
         <p>
-          <a id="bt-email" className="bt" href="mailto:albemarleballettheatre@gmail.com" target="_top">
+          <a
+            id="bt-email"
+            className="bt"
+            href="mailto:albemarleballettheatre@gmail.com"
+            target="_top"
+          >
             <Mail /> albemarleballettheatre@gmail.com
           </a>
         </p>
@@ -21,7 +27,12 @@ const Contact = () => (
           </a>
         </p>
         <p>
-          <a className="bt" href="https://www.facebook.com/AlbemarleBallet" target="_blank" rel="noopener noreferrer">
+          <a
+            className="bt"
+            href="https://www.facebook.com/AlbemarleBallet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Facebook /> Visit us on Facebook
           </a>
         </p>
@@ -35,11 +46,15 @@ const Contact = () => (
       <div className="clear" />
       <div className="map-wrap">
         <p>Directions to our Studio:</p>
-        <iframe title="ABT Adress" frameBorder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3141.0082499261734!2d-78.70194868530913!3d38.07018897970674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b37fab9e8c95e3%3A0x2a61bbc7bcd57c38!2sAlbemarle+Ballet+Theatre!5e0!3m2!1sen!2sus!4v1517544462711" allowFullScreen />
+        <iframe
+          title="ABT Adress"
+          frameBorder="0"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3141.0082499261734!2d-78.70194868530913!3d38.07018897970674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b37fab9e8c95e3%3A0x2a61bbc7bcd57c38!2sAlbemarle+Ballet+Theatre!5e0!3m2!1sen!2sus!4v1517544462711"
+          allowFullScreen
+        />
       </div>
     </div>
   </div>
-
 )
 
 export default Contact

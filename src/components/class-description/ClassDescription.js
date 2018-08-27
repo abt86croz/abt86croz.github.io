1c975cb304a49fe59b5efde90d2f8e5f67b48e19
Altamir <alt.coelho@gmail.com>
2018-08-27 11:03:41 -0500

    edit title and descriptions

--- src/components/class-description/ClassDescription.js
+++ src/components/class-description/ClassDescription.js
@@ -17,9 +17,9 @@ const Description = () => (
           and professionalism.
         </p>
       </div>
-      <p className="heading bold">Class Descriptions</p>
+      <p className="heading">Class Descriptions</p>
       <div className="description">
-        <p className="sub-title">EXPLORING BALLET • Ages 3-4 • 45-minutes</p>
+        <p className="sub-title bold">EXPLORING BALLET • Ages 3-4 • 45-minutes</p>
         <p>
           Your preschool dancer’s very first peek at classical ballet! Our
           unique curriculum teaches and explores body positions, fundamental
@@ -29,7 +29,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">BALLET IA • Ages 5-6 • 1 hour</p>
+        <p className="sub-title bold">BALLET IA • Ages 5-6 • 1 hour</p>
         <p>
           An imaginative class that uses creative methods and props to introduce
           your beginner dancer to arm and foot positions, body placement and
@@ -40,12 +40,12 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">BALLET IB • Ages 7-8 • 1 hour</p>
+        <p className="sub-title bold">BALLET IB • Ages 7-8 • 1 hour</p>
         <p>This is the same as Ballet 1A, tailored for this age group. </p>
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           Ballet IC (Open Ballet) • Ages 9 to Adult • 1 hour
         </p>
         <p>
@@ -58,7 +58,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">BALLET IIA • Beginner Intermediate • 1 hour</p>
+        <p className="sub-title bold">BALLET IIA • Beginner Intermediate • 1 hour</p>
         <p>
           This curriculum is designed to help young dancers develop, learn, and
           feel empowered in their dance experience. Classes expand on the
@@ -69,7 +69,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           BALLET IIB • Beginner Intermediate • 1 hour 15 minutes
         </p>
         <p>
@@ -79,7 +79,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           BALLET IIIA • Advanced Intermediate • Pre-Pointe • 1 hour 30 minutes
         </p>
         <p>
@@ -90,7 +90,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">BEGINNER POINTE • 1 hour</p>
+        <p className="sub-title bold">BEGINNER POINTE • 1 hour</p>
         <p>
           For students in their first two years of pointe work. This class
           focuses on strength, stability, strong emphasis on correct use of the
@@ -106,7 +106,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           BALLET IIIB • Advanced Intermediate • Pre-Pointe and Pointe • 2 hours
         </p>
         <p>
@@ -121,7 +121,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">BALLET IV • Beginner Advanced • 2 hours</p>
+        <p className="sub-title bold">BALLET IV • Beginner Advanced • 2 hours</p>
         <p>
           A multi-level advanced class tailored for the graduating Ballet IIIB
           or any advanced or pre-professional student. This class offers the
@@ -133,7 +133,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">ADVANCED BALLET • 2 hours</p>
+        <p className="sub-title bold">ADVANCED BALLET • 2 hours</p>
         <p>
           For the advanced, pre-professional, or professional dancer. In
           addition to barre and center work, these classes offer pointe work,
@@ -144,7 +144,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">ADVANCED POINTE • 1 hour 15 minutes</p>
+        <p className="sub-title bold">ADVANCED POINTE • 1 hour 15 minutes</p>
         <p>
           For students who have trained a minimum of two years en pointe, upon
           the recommendation of a teacher.
@@ -158,7 +158,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">HOMESCHOOL BALLET • Ages 5+ • 1 hour</p>
+        <p className="sub-title bold">HOMESCHOOL BALLET • Ages 5+ • 1 hour</p>
         <p>
           An open level ballet class for homeschool students with daytime
           scheduling needs and availability.
@@ -166,7 +166,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           BEGINNER/INTERMEDIATE JAZZ/MODERN CLASS • 1 hour and 1 hour 15 minutes
         </p>
         <p>
@@ -180,7 +180,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           ADVANCED JAZZ/MODERN CLASS • 1 hour 15 minutes
         </p>
         <p>
@@ -193,7 +193,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">MAT PILATES • 1 hour</p>
+        <p className="sub-title bold">MAT PILATES • 1 hour</p>
         <p>
           Pilates is a system of controlled exercises that engage the mind and
           condition the whole body. The blend of strength and flexibility
@@ -209,7 +209,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           BABY AND ME BARRE BOOTCAMP • BABY WEARING • 45 minutes
         </p>
         <p>
@@ -227,7 +227,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">CREATIVE MOVEMENT • Ages 2-4 • 45 minutes</p>
+        <p className="sub-title bold">CREATIVE MOVEMENT • Ages 2-4 • 45 minutes</p>
         <p>
           Explore the world of movement and music, and develop motor skills and
           rhythm through movement games, nursery rhymes, and songs! Fun props
@@ -237,7 +237,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">DANCE WITH ME• Ages 2-4 • 45 minutes</p>
+        <p className="sub-title bold">DANCE WITH ME• Ages 2-4 • 45 minutes</p>
         <p>
           The same as Creative Movement with a parent or guardian participation.
           One adult per child. Adult participation necessary.
@@ -251,7 +251,7 @@ const Description = () => (
       </div>
       <hr />
       <div className="description">
-        <p className="sub-title">
+        <p className="sub-title bold">
           MOVEMENT THERAPY THROUGH DANCE • Ages 2-8 • 1 hour
         </p>
         <p>

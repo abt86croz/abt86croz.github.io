ed0af1259d6f2a0a3df174868e7bce27e22fc1f2
abt86croz <36944810+abt86croz@users.noreply.github.com>
2019-01-10 17:36:03 -0600

    Class listings (#7)
    * update class schedule
    
    * redesign navbar
    
    * update class listings
    
    * update package json
    
    * class listings build changes
    
    * resedesign class listings tables
    
    * replace dependecies
    
    * tables

--- src/components/class-schedule/ClassSchedule.js
+++ src/components/class-schedule/ClassSchedule.js
@@ -3,8 +3,10 @@ import PDFIcon from 'react-icons/lib/fa/file-pdf-o'
 import RightArrow from 'react-icons/lib/fa/angle-double-right'
 import { Link } from 'react-router-dom'
 import { IphoneImage } from '../Images'
-import './ClassSchedule.css'
 import ClassSchePDF from '../../pdf/class-schedule.pdf'
+import ClassListings from './ClassListings'
+import './ClassSchedule.css'
+import './ClassListings.css'
 
 /* eslint-disable max-len */
 const Schedule = () => (
@@ -31,57 +33,7 @@ const Schedule = () => (
           scrolling="no"
         />
       </div>
-      <div className="clear" />
-      <div className="iframe-container">
-        <p className="heading show">Beginning Ballet | January 3 - May 3, 2019</p>
-        <iframe
-          className="iframe-listing"
-          src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsDirect?OrgID=534011&Cat2=Beginning%20Ballet&hidecols=Gender,Ages,EndDate,Session,openings&sort=class"
-          frameBorder="0"
-          allowFullScreen
-          title="ABT Class Schedule"
-        />
-      </div>
-      <div className="clear" />
-
-      <div className="iframe-container">
-        <p className="heading show">8-Week Sessions</p>
-        <span className="subtitle-listing">Session 1: January 3 – February 27, 2019</span>
-        <span className="subtitle-listing">Session 2: February 28 – May 1, 2019</span>
-        <iframe
-          className="iframe-listing"
-          src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsDirect?OrgID=534011&Cat1=8-Week%20Sessions&hidecols=Gender,Ages,EndDate,Session,Openings&sort=days"
-          frameBorder="0"
-          allowFullScreen
-          title="ABT Class Schedule"
-        />
-      </div>
-      <div className="clear" />
-
-      <div className="iframe-container">
-        <p className="heading show">By Placement Only | January 3 – May 3, 2019</p>
-        <iframe
-          className="iframe-listing placement"
-          src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsDirect?OrgID=534011&Cat2=by%20placement%20only&hidecols=Gender,Ages,EndDate,Session,Openings,Class%20Starts&sort=days&sort=class"
-          frameBorder="0"
-          allowFullScreen
-          title="ABT Class Schedule"
-        />
-      </div>
-      <div className="clear" />
-
-      <div className="iframe-container">
-        <p className="heading show">Mat Pilates | 1-Month Sessions</p>
-        <iframe
-          className="iframe-listing"
-          src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsDirect?OrgID=534011&Cat2=fitness&hidecols=Description,Gender,Ages,EndDate,Session,Openings&sort=days&sort=class"
-          frameBorder="0"
-          allowFullScreen
-          title="ABT Class Schedule"
-        />
-      </div>
-      <div className="clear" />
-
+      <ClassListings />
       <div className="btn-wrap">
         <a className="btn-sm bt" href={ClassSchePDF} target="_blank" rel="noopener noreferrer">
           <PDFIcon /> Print Weekly Class Schedule

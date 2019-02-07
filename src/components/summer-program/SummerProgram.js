8ae8f4e1247f6e1da6578da261bada9abf7b317e
rimatla <alt.coelho@gmail.com>
2019-02-07 12:38:55 -0600

    update summer page

--- src/components/summer-program/SummerProgram.js
+++ src/components/summer-program/SummerProgram.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PDFIcon from 'react-icons/lib/fa/file-pdf-o'
-import SummerRegistrationForm from '../../pdf/2018SummerRegistration.pdf'
+import SummerClassSched from '../../pdf/2019-summer-class-schedule.pdf'
+import SummerCampSched from '../../pdf/2019-summer-camp-schedule.pdf'
 import './SummerProgram.css'
 import { SummerPosterImage } from '../Images'
 
@@ -12,17 +13,43 @@ const SummerProgram = () => (
     </div>
     <div className="summer-content-wrap">
       <div className="summer-content">
-        <p className="title">Summer Camp and Class Descriptions</p>
-        <p className="sub-title">Exploring Ballet - Ages 3-5</p>
-        <p className="date">August 6 - 9</p>
-        <p>A young dancer&apos;s very first peek at classical ballet! This camp teaches ballet technique, flexibility, coordination, balance, creative movement and games, and stories of the classical ballets. The dancers decorate their own costumes (that they keep after the camp), and share a demonstration recital on the last day. Craft supplies & snacks are included.</p>
-        <p className="sub-title">* The Young Dancer Camp - Ages 5-7 and 8+</p>
-        <p className="date">July 9 - 13  &  July 16 - 20</p>
-        <p>These well-rounded camps are for the young dancer that would like to explore and experience the world of ballet and dance. They offer ballet technique, jazz, modern, theatre, tango, Pilates, stretching, stage make-up, and sewing.</p>
-        <p className="sub-title"> * Dance Conservatory: Intermediate and Advanced/Pre-Professional</p>
-        <p className="date">July 23 - 27  &  July 30 - August 3</p>
-        <p>For dancers that would like to fully immerse themselves in the world of ballet and dance, and take a more serious step with their training. These camps offer ballet technique, pointe*, variations, jazz, modern, character, theatre, social dancing, Pilates, stretching, and stage make-up.
-        * For dancers who are already en pointe prior to signing up for the camp.Dancers who are not yet dancing en pointe may take the pointe classes as pre - pointe, and will gain strength in preparation for pointe work.</p>
+        <p className="title">Summer Camps</p>
+
+        {/* Exploring Ballet Camp */}
+        <p className="sub-title">Exploring Ballet | Ages 3-5 | $175</p>
+        <p className="date">August 5 - 8 | August 12 – 15 | Monday - Thursday | 10am - 12pm</p>
+        <p>
+          A young dancer&apos;s very first peek at classical ballet! This camp teaches ballet
+          technique, flexibility, coordination, balance, creative movement and games, and stories of
+          the classical ballets. The dancers decorate their own costumes (that they keep after the
+          camp), and share a demonstration recital on the last day. Craft supplies & snacks are
+          included.
+        </p>
+
+        {/* Younger Dancer Camp */}
+        <p className="sub-title">The Young Dancer Camp | Ages 6+ and 9+ | $325</p>
+        <p className="date">July 8 - 12 | Beginner | Monday - Friday | Ages 6+ | 9am - 3pm</p>
+        <p className="date">
+          July 15 - 19 | Beginner/Intermediate | Monday - Friday | Ages 9+ | 9am - 3pm
+        </p>
+        <p>
+          These well-rounded camps are for the young dancer that would like to explore and
+          experience the world of ballet and dance.They offer ballet technique, jazz, modern,
+          theatre, West Coast Swing, Tango, Pilates, and conditioning and stretching.
+        </p>
+
+        {/* Dance Conservatory Camp */}
+        <p className="sub-title">Dance Conservatory: Intermediate and Advanced/Pre-Professional</p>
+        <p className="date">July 22 – 26 | Intermediate | Monday - Friday | 9am - 4pm | $385</p>
+        <p className="date">July 29 - August 2 | Advanced | Monday - Friday | 9am - 4pm | $335</p>
+        <p>
+          For dancers that would like to fully immerse themselves in the world of ballet and dance,
+          and take a more serious step with their training. These camps offer ballet technique,
+          pointe*, variations, jazz, modern, character, Capoeira, theatre, West Coast Swing, and
+          Pilates. *For dancers who are already en pointe prior to signing up for the camp. Dancers
+          who are not yet dancing en pointe may take the pointe classes as pre-pointe, and will gain
+          strength in preparation for pointe work.
+        </p>
       </div>
     </div>
     <div className="summer-schedule">
@@ -33,12 +60,16 @@ const SummerProgram = () => (
         <p>Ballet IB/C • Ages 8 & Older • Beginners Welcome</p>
         <p>Intermediate Jazz/Modern</p>
         <p>Ballet II • Intermediate Classes</p>
-        <p>Ballet III • Intermediate Classes<br />Pre-Pointe & Beg Pointe</p>
+        <p>
+          Ballet III • Intermediate Classes
+          <br />
+          Pre-Pointe & Beg Pointe
+        </p>
         <p>Advanced Ballet • 2+ Years Pointe Experience</p>
         <p className="head">Camps</p>
         <p>Exploring Ballet Camp for Preschool • Ages 3 - 4</p>
         <p>The Young Dancer Camp • Ages 5 - 7 </p>
-        <p>The Young Dancer Camp  • Ages 8 - 10</p>
+        <p>The Young Dancer Camp • Ages 8 - 10</p>
         <p>Dance Conservatory Intensive • Intermediate</p>
         <p>Dance Conservatory Intensive • Advanced & Pre-Prof.</p>
       </div>
@@ -61,7 +92,12 @@ const SummerProgram = () => (
     </div>
     <div className="clear" />
     <div className="btn-wrap">
-      <a className="bt" href={SummerRegistrationForm} target="_blank" rel="noopener noreferrer"><PDFIcon /> Registration Form</a>
+      <a className="bt" href={SummerClassSched} target="_blank" rel="noopener noreferrer">
+        <PDFIcon /> SUMMER CLASS schedule
+      </a>
+      <a className="bt" href={SummerCampSched} target="_blank" rel="noopener noreferrer">
+        <PDFIcon /> SUMMER CAMP schedule
+      </a>
     </div>
   </div>
 )

7834bd8f4403403e7c6f4b071704c8c2212b23ab
Altamir Coelho <alt.coelho@gmail.com>
2019-07-07 13:52:25 -0500

    Summer19 (#19)
    update content

--- src/Routes.js
+++ src/Routes.js
@@ -13,7 +13,7 @@ import PhotoGallery from './components/photo-gallery/PhotoGallery'
 import Tuition from './components/tuition/Tuition'
 import FinancialAid from './components/financial-aid/FinancialAid'
 import Description from './components/class-description/ClassDescription'
-import NewClasses from './components/class-new/ClassNew'
+import LevelPlacement from './components/level-placement/LevelPlacement'
 import Schedule from './components/class-schedule/ClassSchedule'
 import ClassAttire from './components/class-attire/ClassAttire'
 
@@ -24,7 +24,7 @@ const instructorsPath = '/about/our-instructors'
 const financialAidPath = '/registration/financial-aid'
 const tuitionPath = '/registration/tuition'
 const descriptionPath = '/classes/description'
-const newClassesPath = '/classes/new-classes'
+const levelPlacementPath = '/classes/level-placement'
 const movementTherapyPath = '/classes/movement-therapy'
 const classAttirePath = '/classes/class-attire'
 const summerProgramPath = '/classes/summer-program'
@@ -44,7 +44,7 @@ const Routes = () => (
     <ScrollToTop path={tuitionPath} exact component={Tuition} />
     <Redirect from="/registration/" to={financialAidPath} />
     <ScrollToTop path={classSchedulePath} component={Schedule} />
-    <ScrollToTop path={newClassesPath} component={NewClasses} />
+    <ScrollToTop path={levelPlacementPath} component={LevelPlacement} />
     <ScrollToTop path={descriptionPath} component={Description} />
     <ScrollToTop path={movementTherapyPath} component={MovementTherapy} />
     <ScrollToTop path={classAttirePath} component={ClassAttire} />

2b2200ce36d8a566edfc1ab498dda56e042b84f6
rimatla <alt.coelho@gmail.com>
2020-01-25 12:04:46 -0600

    2020 spring

--- src/components/performances/Performances.js
+++ src/components/performances/Performances.js
@@ -2,10 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import Content from '../styles/Content'
 import Title from '../styles/Title'
-import StoryImg from '../../images/site/step-into-story.png'
-import NutImg from '../../images/site/nutcracker-ticket.png'
+// import StoryImg from '../../images/site/step-into-story.png'
+// import NutImg from '../../images/site/nutcracker-ticket.png'
+import StepIntoStoryIMG from '../../images/site/StepIntoTheStory.png'
 
-import PayPalNutCracker from '../paypal/Paypal'
+// import PayPalNutCracker from '../paypal/Paypal'
 import './Performances.css'
 
 // import springGalaImg from '../../images/site/2019-spring-gala.png'
@@ -53,6 +54,7 @@ const Performances = () => (
         <p className="sub-title bold">2019-2020 performance dates coming soon…</p>
       </Description> */}
 
+      {/* Coming Back in some point
       <Description>
         <Show>
           <p className="bold underline">Nutcracker Community Outreach Performances</p>
@@ -96,6 +98,7 @@ const Performances = () => (
         </Show>
         <hr />
       </Description>
+*/}
 
       <Description>
         <Show>
@@ -114,6 +117,11 @@ const Performances = () => (
             <p>Saturday, May 9, 2020 | 2:00pm &amp; 6:00pm</p>
             <p>The Dickinson Theatre at Piedmont Community College, Charlottesville, VA </p>
           </Location>
+          <Location>
+            <a href="https://v6.click4tix.com/event-details.php?e=431204" target="_blank" rel="noopener noreferrer">
+              <img src={StepIntoStoryIMG} alt="The Nutcracker Performances" />
+            </a>
+          </Location>
         </Show>
         <hr />
       </Description>
@@ -122,7 +130,14 @@ const Performances = () => (
         <Show>
           <p className="bold underline">Spring Gala Community Outreach Performances</p>
           <Location>
-            <p>Two performances will be scheduled. Dates and times TBA.</p>
+            <p>Monday, April 27, 2020 | 5:00pm </p>
+            <p>Crozet Library, Crozet, VA </p>
+            <p>Ballet IV - Advanced levels invited to perform </p>
+          </Location>
+
+          <Location>
+            <p>Monday, April 29, 2020 | 4:00pm </p>
+            <p>The Lodge at Old Trail, Crozet, VA </p>
             <p>Ballet IV - Advanced levels invited to perform </p>
           </Location>
         </Show>
@@ -131,9 +146,9 @@ const Performances = () => (
 
       <Description primary>
         <p>
-          Our dance studio is conveniently located in Crozet, near Western Albemarle High School,
-          Henley, Brownsville, and Crozet Elementary Schools, minutes away from Charlottesville, and
-          near Greene County, Nelson County, Orange County and Waynesboro.
+          Our dance studio is conveniently located in Crozet, near Western Albemarle High School, Henley, Brownsville,
+          and Crozet Elementary Schools, minutes away from Charlottesville, and near Greene County, Nelson County,
+          Orange County and Waynesboro.
         </p>
         <p className="italic">
           For more information: <br /> email us at

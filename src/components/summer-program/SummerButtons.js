f008e0a1e0dd9f4a9bb140775058cbb671dde9a4
abt86croz <albemarleballettheatre@gmail.com>
2024-01-21 23:04:24 -0500

    summer sche

--- src/components/summer-program/SummerButtons.js
+++ src/components/summer-program/SummerButtons.js
@@ -1,8 +1,10 @@
-import React from 'react'
+/* eslint-disable quotes */
+/* eslint-disable semi */
+import React from "react";
 
-import PDFIcon from 'react-icons/lib/fa/file-pdf-o'
-import SummerClassSched from '../../pdf/Summer2023-Schedule.pdf'
-import SummerHandout from '../../pdf/summer-handout.pdf'
+import PDFIcon from "react-icons/lib/fa/file-pdf-o";
+import SummerClassSched from "../../pdf/summer-schedule.pdf";
+import SummerHandout from "../../pdf/summer-handout.pdf";
 
 const SummerButtons = () => (
   <div>
@@ -16,6 +18,6 @@ const SummerButtons = () => (
       </a>
     </div>
   </div>
-)
+);
 
-export default SummerButtons
+export default SummerButtons;
